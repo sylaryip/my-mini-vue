2f381f237b5aa1fd6101869e009d544648f710ca
yezhihao <yezhihao@topgamesinc.com>
2022-03-28 00:13:12 +0800

    feat: reactive & effect 收集依赖和触发依赖

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,42 @@
+class EffectReactive {
+  private _fn;
+  constructor(fn) {
+    this._fn = fn;
+  }
+  run() {
+    activeEffect = this;
+    this._fn();
+  }
+}
+
+export function effect(fn) {
+  const effectReactive = new EffectReactive(fn);
+
+  effectReactive.run();
+}
+
+let activeEffect;
+
+const targetMap = new Map();
+export function track(target, key) {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+  dep.add(activeEffect);
+}
+
+export function trigger(target, key) {
+  const depsMap = targetMap.get(target);
+  const dep = depsMap.get(key);
+
+  for (const effect of dep) {
+    effect.run();
+  }
+}
